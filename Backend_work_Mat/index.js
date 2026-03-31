432ac1d0946ea3f19621fef2ae91cd6f95ec5d0b
Matheusfr25 <mataraujo10.o@gmail.com>
2026-03-30 23:14:17 -0300

    Update index.js

--- Backend_work_Mat/index.js
+++ Backend_work_Mat/index.js
@@ -3,15 +3,15 @@
 const express = require('express'); 
 const app = express();
 const PORT = 3001;
-const produtosRoutes = require('./routes/produtos'); //exporta as rotas
+const produtosRoutes = require('./routes/produtos'); // Exporta as routes
 
 app.use(express.json())
 
-app.get('/', (req, res) => { // Rota para enviar texto no diretório 
+app.get('/', (req, res) => { // Route para enviar texto no diretório 
     res.send('Servidor da API de estoque está rodando com sucesso!');
 });
 
-app.use('/produtos', produtosRoutes); //Define que as rotas dos produtos teram 
+app.use('/produtos', produtosRoutes); // Define o que as routes dos produtos tem 
 
 app.listen(PORT, () => { // Para mostrar aonde o servidor está rodando no terminal 
     console.log(`Servidor rodando na porta http://localhost:${PORT}`);
